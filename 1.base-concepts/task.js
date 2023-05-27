1a6a71fa51457e9265e5fa94310a8df4c79db004
Dan <sarychev-daniil@yandex.ru>
2023-05-27 14:05:12 +0300

    Homework was corrected

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,4 +1,5 @@
 "use strict";
+
 function solveEquation(a, b, c) {
 	let arr = [];
 	let positiveRootMoreZero;
@@ -9,21 +10,22 @@ function solveEquation(a, b, c) {
 	console.log("Дискриминант равен: " + d);
 
 	if (d < 0) {
-		return arr = [];
+		return arr;
 		console.log("Нет корней уровнения" + arr);
 	} else if (d > 0) {
 		positiveRootMoreZero = (-b + Math.sqrt(d)) / (2 * a);
 		negativeRootMoreZero = (-b - Math.sqrt(d)) / (2 * a);
 		arr.push(positiveRootMoreZero, negativeRootMoreZero);
 		console.log("Корень уравнения х1: " + arr[0] + "," + " Корень уравнения х2: " + arr[1]);
-		return arr = [positiveRootMoreZero, negativeRootMoreZero];
+		return arr;
 
 	} else(d === 0) 
-  {
+	
+	{
 		rootEqualsZero = -b / (2 * a);
 		arr.push(rootEqualsZero);
 		console.log("Корень уравнения х: " + arr[0]);
-		return arr = [rootEqualsZero];
+		return arr;
 
 	}
 }
@@ -33,34 +35,14 @@ function solveEquation(a, b, c) {
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
 
 
-	if (Number.isNaN(percent)) {
-		return false;
-	} else if (Number.isNaN(contribution)) {
-
-		return false;
-
-	} else if (Number.isNaN(amount)) {
-
-		return false;
-	} else if (Number.isNaN(countMonths)) {
-
-		return false;
-
-	} else {
-
-
-
-
-
-		let indexRange = percent / 100;
-		console.log("Ставка от 0 до 1: " + indexRange);
-		let rateInMonth = indexRange / 12;
-		console.log("Месячная ставка : " + rateInMonth);
-		let creditBody = amount - contribution;
-		console.log("Тело кредита : " + creditBody);
-		let paymentInMonth = creditBody * (rateInMonth + (rateInMonth / (((1 + rateInMonth) ** countMonths) - 1)));
-		let payment = paymentInMonth * 12;
-		let resultPayment = (payment).toFixed(2);
-		return parseFloat(resultPayment);
-	}
+	let indexRange = percent / 100;
+	console.log("Ставка от 0 до 1: " + indexRange);
+	let rateInMonth = indexRange / 12;
+	console.log("Месячная ставка : " + rateInMonth);
+	let creditBody = amount - contribution;
+	console.log("Тело кредита : " + creditBody);
+	let paymentInMonth = creditBody * (rateInMonth + (rateInMonth / (((1 + rateInMonth) ** countMonths) - 1)));
+	let payment = paymentInMonth * countMonths;
+	let resultPayment = (payment).toFixed(2);
+	return parseFloat(resultPayment);
 }
